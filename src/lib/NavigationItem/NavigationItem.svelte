d977e64b2155489fb5b9b6b0fdab8091b0e15baf
Olek Łukasiewicz <aleksander_lukasiewicz@outlook.com>
2023-07-22 23:07:27 +0200

    Added navigation bar

---
+++ src/lib/NavigationItem/NavigationItem.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { page } from '$app/stores';  
+    export let label = "";
+    export let isSelected = false;
+    export let viewId = "";
+</script>
+
+<a
+    href={"/" + viewId}
+    class:selected={$page.route.id=="/"+viewId}
+>
+    <span>{label}</span>
+</a>
+
+<style lang="scss">
+   @import "NavigationItem.scss";
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
   import { browser } from "$app/environment";
+  import NavigationItem from "$lib/NavigationItem/NavigationItem.svelte";
   import "$locales/locales"; // Import to initialize. Important :)
-  import { locale, waitLocale } from "svelte-i18n";
+  import { locale, waitLocale,isLoading,_ } from "svelte-i18n";
 
   export const load = async () => {
     if (browser) {
@@ -10,11 +11,16 @@
     await waitLocale();
   };
 </script>
-
+{#if !$isLoading}
+<div id="nav">
+<NavigationItem label={$_("navigation.appname")} viewId="" />
+<NavigationItem label={$_("navigation.explore")} viewId="explore" />
+<NavigationItem label={$_("navigation.design")}  viewId="item"/>
+</div>
 <div>
   <slot />
 </div>
-
+{/if}
 <style lang="scss">
   @import "layout_style.scss";
 </style>
