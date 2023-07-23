3d2bc690c2d567ac5225de0a1405c6ce2f40baa1
Olek Łukasiewicz <aleksander_lukasiewicz@outlook.com>
2023-07-23 12:06:20 +0200

    Nav changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
   import { browser } from "$app/environment";
   import NavigationItem from "$lib/NavigationItem/NavigationItem.svelte";
   import "$locales/locales"; // Import to initialize. Important :)
-  import { locale, waitLocale,isLoading,_ } from "svelte-i18n";
+  import { locale, waitLocale, isLoading, _ } from "svelte-i18n";
 
   export const load = async () => {
     if (browser) {
@@ -11,16 +11,19 @@
     await waitLocale();
   };
 </script>
+
 {#if !$isLoading}
-<div id="nav">
-<NavigationItem label={$_("navigation.appname")} viewId="" />
-<NavigationItem label={$_("navigation.explore")} viewId="explore" />
-<NavigationItem label={$_("navigation.design")}  viewId="item"/>
-</div>
-<div>
-  <slot />
-</div>
+  <div id="nav">
+    <div id="nav-title">{$_("navigation.appname")}</div>
+    <NavigationItem label={$_("navigation.home")} viewId="" />
+    <NavigationItem label={$_("navigation.explore")} viewId="explore" />
+    <NavigationItem label={$_("navigation.design")} viewId="design" />
+  </div>
+  <div>
+    <slot />
+  </div>
 {/if}
+
 <style lang="scss">
   @import "layout_style.scss";
 </style>
