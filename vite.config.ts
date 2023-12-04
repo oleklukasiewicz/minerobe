df8bda6cb22748f7ea14ccffb97a53e6bf32613e
Olek Łukasiewicz <aleksander_lukasiewicz@outlook.com>
2023-12-04 21:01:19 +0100

    changes to server

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-import dotenv from 'dotenv';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+import dotenv from "dotenv";
 
 // Load environment variables from .env
 dotenv.config();
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	define: {
-		'process.env': import.meta.env
-	  }
+  plugins: [sveltekit()],
+  define: {
+    "process.env": import.meta.env,
+  },
+  server: {
+    /*here*/
+    hmr: { overlay: false },
+  },
 });
