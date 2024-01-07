c45f160bf9c35f02cd830fef8a33215576443ae3
Olek Łukasiewicz <aleksander_lukasiewicz@outlook.com>
2024-01-07 20:22:05 +0100

    changes

--- src/routes/api/service/set_skin/[id]/[model]/+server.ts
+++ src/routes/api/service/set_skin/[id]/[model]/+server.ts
@@ -2,6 +2,11 @@ import { ChangeSkin } from "$lib/server/minecraftservice";
 
 export const GET = async (event) => {
   const request = event.request;
+  try{
   await ChangeSkin(event.params.id, event.params.model);
+  } catch (e) {
+    console.log(e);
+    return new Response(JSON.stringify(e), { status: 500 });
+  }
   return new Response("OK");
 };
