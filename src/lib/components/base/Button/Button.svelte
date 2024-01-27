9a64a71fc7d2529a2e7b06a00e468c04c32f64e5
Olek Łukasiewicz <aleksander_lukasiewicz@outlook.com>
2024-01-27 23:09:51 +0100

    buttons test

---
+++ src/lib/components/base/Button/Button.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  export let href: string = null;
+  export let label: string = null;
+  export let icon: string = null;
+  export let type: "primary" | "secondary" | "tertiary" = "primary";
+  export let size: "small" | "medium" | "large" = "medium";
+  export let iconSize: "small" | "medium" | "large" = size;
+  export let disabled: boolean = false;
+  export let onlyIcon: boolean = false;
+</script>
+
+<a
+  class="button"
+  {href}
+  class:link={href != null}
+  class:icon={onlyIcon && icon != null}
+  class:with-label={label != null && !onlyIcon}
+  class:primary={type === "primary"}
+  class:secondary={type === "secondary"}
+  class:tertiary={type === "tertiary"}
+  class:small={size === "small"}
+  class:medium={size === "medium"}
+  class:large={size === "large"}
+  class:disabled
+>
+  {#if icon != null}
+    <div
+      class="icon"
+      class:b-icon-small={iconSize === "small"}
+      class:b-icon-medium={iconSize === "medium"}
+      class:b-icon-large={iconSize === "large"}
+    >
+      {@html icon}
+    </div>
+  {/if}
+  {#if label != null && !onlyIcon}
+    <span>{label}</span>
+  {/if}
+</a>
+
+<style lang="scss">
+  @import "Button.scss";
+</style>
--- src/routes/explore/+page.svelte
+++ src/routes/explore/+page.svelte
@@ -1,39 +1,40 @@
 <script lang="ts">
-  import OutfitPicker from "$component/outfit/OutfitPicker/OutfitPicker.svelte";
-  import { appState, wardrobe } from "$src/data/cache";
-  import { APP_STATE } from "$src/data/consts";
-  import { FetchWardrobeOutfitsByCategory } from "$src/helpers/apiHelper";
-  import { GetCategoriesFromList } from "$src/helpers/imageDataHelpers";
-  let outfits = [];
-  let categories = ["ALL"];
-  let isPickerLoading = true;
-  appState.subscribe(async (state) => {
-    if (state == APP_STATE.READY) {
-      const categoryCounts = GetCategoriesFromList($wardrobe.outfits);
-      categories = Object.keys(categoryCounts).filter(
-        (x) => categoryCounts[x] > 0
-      );
-      outfits = await FetchWardrobeOutfitsByCategory("ALL");
-      isPickerLoading = false;
-    }
-  });
-  const fetchByCategory = async function (e) {
-    isPickerLoading = true;
-    outfits = await FetchWardrobeOutfitsByCategory(e.detail);
-    isPickerLoading = false;
-  };
+  import Button from "$lib/components/base/Button/Button.svelte";
+  import DownloadIcon from "$icons/download.svg?raw";
 </script>
 
 <h1 id="view-title">Explore new outfits</h1>
-{#if $appState == APP_STATE.READY}
-  <OutfitPicker
-    bind:loading={isPickerLoading}
-    {outfits}
-    {categories}
-    on:category={fetchByCategory}
-  />
-{/if}
-
+<Button label="Button" />
+<Button label="Button" size="large" />
+<Button label="Button" size="small" />
+<br />
+<Button label="Button" type="secondary" />
+<Button label="Button" type="secondary" size="large" />
+<Button label="Button" type="secondary" size="small" />
+<br />
+<Button label="Button" type="tertiary" />
+<Button label="Button" type="tertiary" size="large" />
+<Button label="Button" type="tertiary" size="small" />
+<br />
+<Button label="Button" icon={DownloadIcon} />
+<Button label="Button" icon={DownloadIcon} size="large"/>
+<Button label="Button" icon={DownloadIcon} size="small"/>
+<br />
+<Button label="Button" icon={DownloadIcon} type="secondary" />
+<Button label="Button" icon={DownloadIcon} type="secondary" size="large"/>
+<Button label="Button" icon={DownloadIcon} type="secondary" size="small"/>
+<br />
+<Button label="Button" icon={DownloadIcon} onlyIcon />
+<Button label="Button" icon={DownloadIcon} onlyIcon size="large"/>
+<Button label="Button" icon={DownloadIcon} onlyIcon size="small"/>
+<br />
+<Button label="Button" icon={DownloadIcon} onlyIcon type="secondary" />
+<Button label="Button" icon={DownloadIcon} onlyIcon type="secondary" size="large"/>
+<Button label="Button" icon={DownloadIcon} onlyIcon type="secondary" size="small"/>
+<br />
+<Button label="Button" icon={DownloadIcon} onlyIcon type="tertiary"/>
+<Button label="Button" icon={DownloadIcon} onlyIcon type="tertiary" size="large"/>
+<Button label="Button" icon={DownloadIcon} onlyIcon type="tertiary" size="small"/>
 <style lang="scss">
   @import "style.scss";
 </style>
