f54d48b9f651cccce06ec407206bdec43d9bfb0d
Aleksander Łukasiewicz <aleksander_lukasiewicz@outlook.com>
2024-03-16 11:13:41 +0100

    pusher fix

--- src/lib/server/services/socketService.ts
+++ src/lib/server/services/socketService.ts
@@ -1,9 +1,9 @@
 import Pusher from "pusher";
 
 export const pusherServer = new Pusher({
-  appId: import.meta.env.PUSHER_APP_ID,
+  appId: import.meta.env.VITE_PUSHER_APP_ID,
   key: import.meta.env.VITE_PUSHER_KEY,
-  secret: import.meta.env.PUSHER_SECRET,
+  secret: import.meta.env.VITE_PUSHER_SECRET,
   cluster: import.meta.env.VITE_PUSHER_CLUSTER,
   useTLS: true,
 });
