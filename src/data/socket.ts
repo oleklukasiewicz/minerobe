60eb6fc320da8beeab7f438ff889ac2e5f59afab
Olek Łukasiewicz <aleksander_lukasiewicz@outlook.com>
2024-04-19 16:51:32 +0200

    changes

--- src/data/socket.ts
+++ src/data/socket.ts
@@ -8,6 +8,7 @@ export const configureSocket = (userId) => {
   const io = ioClient(
     get(page).url.hostname + (import.meta.env.VITE_IS_DEV==true ? ":4173" : "")
   );
+  console.log("Connecting to server", import.meta.env.VITE_IS_DEV);
   io.on("connect", () => {
     console.log("Connected to server");
     io.emit("join", userId);
