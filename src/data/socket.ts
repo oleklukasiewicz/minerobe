70faea49fdac29279b2219d507e253f21397de87
Olek Łukasiewicz <aleksander_lukasiewicz@outlook.com>
2024-04-19 16:31:45 +0200

    change

--- src/data/socket.ts
+++ src/data/socket.ts
@@ -5,7 +5,7 @@ import { userSettings } from "./cache";
 import ioClient from "socket.io-client";
 
 export const configureSocket = (userId) => {
-  const io = ioClient(get(page).url.hostname);
+  const io = ioClient(get(page).url.hostname + ":4173");
   io.on("connect", () => {
     console.log("Connected to server");
     io.emit("join", userId);
