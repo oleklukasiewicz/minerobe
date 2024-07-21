23e5403ebefb5df948e542e099a3d961ed36e05d
Aleksander Łukasiewicz <aleksander_lukasiewicz@outlook.com>
2024-07-21 18:22:11 +0200

    removed unused configurations

--- minerobe.api/Configuration/MicrosoftAuthConfig.cs
+++ minerobe.api/Configuration/MicrosoftAuthConfig.cs
@@ -3,8 +3,6 @@
     public class MicrosoftAuthConfig
     {
         public string ClientId { get; set; }
-        public string CodeVerifier { get; set; }
-        public string RedirectEndpoint { get; set; }
         public string OriginUri { get; set; }
     }
 }
