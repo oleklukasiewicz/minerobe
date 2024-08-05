7f838b30e961eb8bc9599a541bb2ee08d4f1a234
Olek Łukasiewicz <aleksander_lukasiewicz@outlook.com>
2024-08-05 22:23:25 +0200

    job timming change

--- minerobe.api/JobsConfig.cs
+++ minerobe.api/JobsConfig.cs
@@ -6,7 +6,7 @@ namespace minerobe.api
     {
         public static void StartJobs(this IApplicationBuilder app)
         {
-            RecurringJob.AddOrUpdate<minerobe.api.Jobs.IXboxJavaAuthRefresh>("xboxJavaRefresh", x => x.Refresh(), "*/30 * * * *");
+            RecurringJob.AddOrUpdate<minerobe.api.Jobs.IXboxJavaAuthRefresh>("xboxJavaRefresh", x => x.Refresh(), "*/15 * * * *");
         }
     }
 }
