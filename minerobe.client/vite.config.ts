a06dd0d76299022fff2420a7a5bcaab543d4c6b0
Aleksander Łukasiewicz <aleksander_lukasiewicz@outlook.com>
2024-12-07 08:20:09 +0100

    backend url moved to env

--- minerobe.client/vite.config.ts
+++ minerobe.client/vite.config.ts
@@ -1,23 +1,25 @@
 import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import dotenv from "dotenv";
 
 // Load environment variables from .env
 dotenv.config();
 
-export default defineConfig({
-  plugins: [sveltekit()],
-  define: {
-    "process.env": import.meta.env,
-  },
-  server:
-  {
-    proxy: {
-      "/api/": {
-        target: "http://localhost:44373/",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [sveltekit()],
+    define: {
+      "process.env": import.meta.env,
+    },
+    server: {
+      proxy: {
+        "/api/": {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
       },
     },
-  }
+  };
 });
