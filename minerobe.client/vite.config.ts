c77164fa30b162a86537ec65f867569516797d82
Olek Łukasiewicz <aleksander_lukasiewicz@outlook.com>
2025-03-15 19:27:35 +0100

    fixes to build

--- minerobe.client/vite.config.ts
+++ minerobe.client/vite.config.ts
@@ -1,13 +1,28 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig, loadEnv } from "vite";
 import dotenv from "dotenv";
+import { visualizer } from "rollup-plugin-visualizer";
 // Load environment variables from .env
 dotenv.config();
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
   return {
-    plugins: [sveltekit()],
+    optimizeDeps: {
+      exclude: ["three", "firebase/auth", "signalr"],
+    },
+    build: {
+      rollupOptions: {
+        external: ["three", "firebase/auth", "signalr"],
+      },
+    },
+    plugins: [
+      sveltekit(),
+      visualizer({
+        emitFile: true,
+        filename: "stats.html",
+      }),
+    ],
     define: {
       "process.env": import.meta.env,
     },
