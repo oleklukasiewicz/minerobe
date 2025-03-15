17374be109ab8725027e1030639e28bfee5a2c4e
Olek Łukasiewicz <aleksander_lukasiewicz@outlook.com>
2025-03-15 17:47:11 +0100

    revert

--- minerobe.client/vite.config.ts
+++ minerobe.client/vite.config.ts
@@ -1,15 +1,13 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig, loadEnv } from "vite";
 import dotenv from "dotenv";
-import { threeMinifier } from "@yushijinhun/three-minifier-rollup";
-
 // Load environment variables from .env
 dotenv.config();
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
   return {
-    plugins: [{ ...threeMinifier(), enforce: "pre" }, sveltekit()],
+    plugins: [sveltekit()],
     define: {
       "process.env": import.meta.env,
     },
